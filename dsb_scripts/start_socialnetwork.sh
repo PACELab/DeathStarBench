4deab9fba18897f5033bfdc3c1dd52af53b669c1
sjindal94 <shubhamjindal_1994@yahoo.com>
2019-10-21 00:20:39 +0000

    Stable commit

--- dsb_scripts/start_socialnetwork.sh
+++ dsb_scripts/start_socialnetwork.sh
@@ -1,4 +1,5 @@
 cd ~/DeathStarBench/socialNetwork/
 sudo docker stack deploy --compose-file=docker-compose.yml socialnetwork
 sleep 3
+echo "sudo docker node ps $(sudo docker node ls -q) --filter desired-state=Running | uniq"
 sudo docker node ps $(sudo docker node ls -q) --filter desired-state=Running | uniq
